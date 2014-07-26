2c7b4892e1afecef0f2e10676f68bd170288918e
Andrew Skotzko <andrew.skotzko@gmail.com>
2014-07-26 10:53:19 -0700

    add snippets on top of prelude + my own init file

--- personal/init-custom.el
+++ personal/init-custom.el
@@ -1,11 +1,7 @@
- ;;; package -- summary
+;; mad props to prelude: https://github.com/bbatsov/prelude
 
 ;; set color theme
 (load-theme 'sanityinc-tomorrow-night)
-
-
-
-
 
 ;; make emacs auto-indent globally by default
 (define-key global-map(kbd"RET")'newline-and-indent)
@@ -15,15 +11,18 @@
 ;; fix windmove conflict w/ org-mode
 (setq org-replace-disputed-keys t)
 
-;; show line numbers
+;; show line numbers globallyo
 (global-linum-mode 1)
 
 ;; NOTES: will this work on normal keyboard setup? did this setup using external keyboard;
 ;; not sure how this will translate to non-mac systems
-;; mac command key act like control key also
+
+;; swap left control and super keys to get nice positioning but
+;; availability of prelude super functionality
 (setq mac-command-modifier 'control)
-;; mac option key act like control key also
-(setq mac-option-modifier 'meta)
+(setq mac-control-modifier 'super)
+;;(setq mac-option-modifier 'meta)
+
 
 ;; lorem ipsum setup
 (autoload 'Lorem-ipsum-insert-paragraphs "lorem-ipsum" "" t)
@@ -33,6 +32,10 @@
 ;; disable the minimize and quit keybindings since running in GUI mode
 (global-unset-key (kbd "C-x C-c"))
 (global-unset-key (kbd "C-x C-z"))
+
+;; unset the built in minimize keys
+(global-unset-key (kbd "C-z"))
+
 
 ;; use inconsolata as default font
 (add-to-list 'default-frame-alist '(font .  "Inconsolata-14"))
@@ -51,13 +54,27 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-;; turn on projectile globally for fast file switching / project nav
-;; *** also make sure that the flx-ido mode is installed, better ido flex matching
-(projectile-global-mode)
+;; require ido & setup flex matching
+(require 'ido)
+(require 'flx-ido)
+(ido-mode t)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
 
+;; setup smex for M-x minibuffer
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
-;;; Commentary:
+;; enable desktop save mode to easily resume work where left off
+(desktop-save-mode 1)
 
-;;; Code:
-
-(provide 'init-local)
+;; bind Control-return to add newline after current one and switch to it
+(global-set-key (kbd "<C-return>") (kbd "C-e C-j")
+)
